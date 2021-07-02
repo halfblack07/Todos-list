0b17bbc9672f7d03c2b0c8f0032aa4d15f4676f4
Ankur <56087410+halfblack07@users.noreply.github.com>
2021-07-02 18:10:32 +0530

    Add files via upload

---
+++ MyComponents/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about My component
+            <p>
+            To get started with React Router in a web app,
+             you’ll need a React web app. If you need to create one,
+              we recommend you try Create React App. It’s a 
+             popular tool that works really well with React Router.
+            </p>
+        </div>
+    )
+}
---
+++ MyComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Footer = () => {
+
+let footerStyle = {
+    position: "absolute",
+    top: "90vh",
+    width: "100%",
+}
+    return (
+        <div className="bg-dark text-light py-3" style={footerStyle}>
+            <p className="text-center">
+           Copyright &copy; MyTodolist.com
+           </p>
+        </div>
+    )
+}
